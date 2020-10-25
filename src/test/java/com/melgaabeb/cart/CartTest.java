57e63d01249aac3e16c199a83cd6f320d63f7cb1
elgaabebmohamed <elgaabeb.mohamed@gmail.com>
2020-10-26 00:29:45 +0100

    Added product add method test

---
+++ src/test/java/com/melgaabeb/cart/CartTest.java
@@ -0,0 +1,32 @@
+package com.melgaabeb.cart;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
+public class CartTest {
+	
+	private Cart cart;
+	
+	@BeforeAll
+	public void setUp() {
+		cart = new Cart();
+	}
+
+	@Test
+	public void WhenProductAddedThenItShouldBeRetainedInTheCart() {
+		
+		Product product = new Product(1L);
+		cart.addProduct(product);
+		
+		assertEquals(cart.getProductList().size(), 1);
+	}
+	
+}
+
