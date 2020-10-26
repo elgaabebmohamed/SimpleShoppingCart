d900dc3edcbeeccdef3a05dca72b3005ac91ee24
elgaabebmohamed <elgaabeb.mohamed@gmail.com>
2020-10-26 11:10:18 +0100

    Added fetch products test

--- src/test/java/com/melgaabeb/cart/CartManagerTest.java
+++ src/test/java/com/melgaabeb/cart/CartManagerTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -97,6 +96,22 @@ public class CartManagerTest {
 		cartManager.addProduct(productThree);
 		
 		assertEquals(cartManager.getTotalPrice() , 5.4);
+
+	}
+	
+	@Test
+	public void WhenFetchProductsIsCalledThenItShouldReturnProductsInTheCart() {
+		
+		Product productOne = new Product(1L, 1.2);
+		cartManager.addProduct(productOne);
+		
+		Product productTwo = new Product(2L, 2.1);
+		cartManager.addProduct(productTwo);
+		
+		Product productThree = new Product(2L, 2.1);
+		cartManager.addProduct(productThree);
+		
+		assertEquals(cartManager.fetchProducts().size() , 3);
 
 	}
 	
